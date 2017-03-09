af1a982cf420483d940e483a54637361fc958653
Hideto Yasunori <hideto0118yasunori@gmail.com>
2017-03-09 02:08:33 -0800

    Modify head about sns

--- header.php
+++ header.php
@@ -5,17 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php wp_title( '|', true, 'right' ); bloginfo('name'); ?></title>
     <meta name="description" content="Hi I'm Hideto who is a front-end developer based in Vancouver. I am seeking a competitive and challenging environment where I can passionately code all day long with my athletic mind and physique.">
+
     <meta property="og:locale" content="en_US" />
     <meta property="og:type" content="website" />
     <meta property="og:title" content="<?php wp_title( '|', true, 'right' ); bloginfo('name'); ?>" />
     <meta property="og:description" content="Hi I'm Hideto who is a front-end developer based in Vancouver. I am seeking a competitive and challenging environment where I can passionately code all day long with my athletic mind and physique." />
-    <meta property="og:url" content="http://webdev-bodymake.com/home/" />
+    <meta property="og:url" content="http://hidetoyasunori.com/" />
     <meta property="og:site_name" content="Hideto Front-End Developer" />
     <meta property=”og:image” content=”<?php echo get_template_directory_uri(); ?>/img/sharedImage.jpg” />
+
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:description" content="Hi I'm Hideto who is a front-end developer based in Vancouver. I am seeking a competitive and challenging environment where I can passionately code all day long with my athletic mind and physique." />
     <meta name="twitter:title" content="Hideto Front-End Developer" />
+    <meta property="”twitter:url" content="http://hidetoyasunori.com/" />
     <meta property=”twitter:image” content=”<?php echo get_template_directory_uri(); ?>/img/sharedImage.jpg” />
+    <meta name="twitter:domain" content="hidetoyasunori.com">
+    <meta name="twitter:image:width" content="672">
+    <meta name="twitter:image:height" content="300">
+
     <link rel="shortcut icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/img/portfolio_favicon.png"/>
     <?php wp_head(); ?>
 </head>
