c203b61a543e86e615cb8c42b8b16df647c38b37
Hideto Yasunori <hideto0118yasunori@gmail.com>
2017-03-09 02:27:32 -0800

    fixed bugs of sns share

--- header.php
+++ header.php
@@ -12,13 +12,13 @@
     <meta property="og:description" content="Hi I'm Hideto who is a front-end developer based in Vancouver. I am seeking a competitive and challenging environment where I can passionately code all day long with my athletic mind and physique." />
     <meta property="og:url" content="http://hidetoyasunori.com/" />
     <meta property="og:site_name" content="Hideto Front-End Developer" />
-    <meta property=”og:image” content=”<?php echo get_template_directory_uri(); ?>/img/sharedImage.jpg” />
+    <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/img/sharedImage.jpg" />
 
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:description" content="Hi I'm Hideto who is a front-end developer based in Vancouver. I am seeking a competitive and challenging environment where I can passionately code all day long with my athletic mind and physique." />
     <meta name="twitter:title" content="Hideto Front-End Developer" />
-    <meta property="”twitter:url" content="http://hidetoyasunori.com/" />
-    <meta property=”twitter:image” content=”<?php echo get_template_directory_uri(); ?>/img/sharedImage.jpg” />
+    <meta property="twitter:url" content="http://hidetoyasunori.com/" />
+    <meta property="twitter:image" content="<?php echo get_template_directory_uri(); ?>/img/sharedImage.jpg" />
     <meta name="twitter:domain" content="hidetoyasunori.com">
     <meta name="twitter:image:width" content="672">
     <meta name="twitter:image:height" content="300">
